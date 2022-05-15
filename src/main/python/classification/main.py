13c64bb0b05cfc8488ca90cb448a0ac0096e1e90
johnfreyes <jhonfer96@hotmail.com>
2022-05-15 18:34:24 -0500

    add vehicle id column to fiel consumption table

--- src/main/python/classification/main.py
+++ src/main/python/classification/main.py
@@ -6,33 +6,64 @@ def getConnection():
     return create_engine('postgresql://postgres:postgrespw@localhost:49153/')
 
 
-def loadDB():
+def getVehiclesId(fuelConsumptionData):
+    allVehicles = findAllVehicles()
+    vehiclesId = []
+    for index, fuelConsumption in fuelConsumptionData.iterrows():
+        vehicleId = allVehicles[(allVehicles['strModelYear'] == fuelConsumption['strModelYear'])
+                                & (allVehicles['strMake'] == fuelConsumption['strMake'])
+                                & (allVehicles['strModel'] == fuelConsumption['strModel'])
+                                & (allVehicles['strClass'] == fuelConsumption['strClass'])]
+        vehiclesId.append(vehicleId.iloc[0]['id'])
+    return vehiclesId
+
+
+def relateVehicleTableToFuelConsumption(fuelConsumptionData):
+    vehiclesId = getVehiclesId(fuelConsumptionData)
+    fuelConsumptionData['vehicleId'] = vehiclesId
+    return fuelConsumptionData
+
+
+def loadVehicleTable(file):
     conn = getConnection()
-    file = "D:\\Users\\jhonf\\Documents\\Programacion\\Codigo\\Python\\ai-fuel-consumption-ratings\\src\\main" \
-           "\\resoures\\MY2022FuelConsumptionRatings.csv"
+    vehicleColumnNames = ['strModelYear', 'strMake', 'strModel', 'strClass']
+    vehicleData = pd.read_csv(file, usecols=vehicleColumnNames)
+    vehicleData = vehicleData.drop_duplicates()
+    vehicleData.to_sql('tbl_vehicle', con=conn, if_exists='replace', index=True, index_label='id')
+
+
+def dropVehicleColumnsToFuelConsumption(fuelConsumptionData):
+    return fuelConsumptionData.drop(fuelConsumptionData.columns[[0, 1, 2, 3]], axis=1)
+
+
+def loadFuelConsumptionTable(file):
+    conn = getConnection()
     vehicleColumnNames = ['strModelYear', 'strMake', 'strModel', 'strClass']
     fuelConsumptionColumnNames = ['douEngineSize_L', 'intCylinders', 'strTransmission', 'strFuelType',
                                   'douFuelConsumption_City_L-100km', 'douFuelConsumption_Hwy_L-100km',
                                   'douFuelConsumption_Comb_L-100km', 'intFuelConsumption_Comb_mpg',
                                   'intCO2Emissions_g-km', 'intCO2Rating', 'intSmogRating']
-    vehicleData = pd.read_csv(file, usecols=vehicleColumnNames)
-    print(vehicleData)
-    fuelConsumptionData = pd.read_csv(file, usecols=fuelConsumptionColumnNames)
-    vehicleData = vehicleData.drop_duplicates()
-    vehicleData.to_sql('tbl_vehicle', if_exists='replace', con=conn)
-    fuelConsumptionData.to_sql('tbl_fuelConsumption', if_exists='replace', con=conn)
+    fuelConsumptionData = pd.read_csv(file, usecols=vehicleColumnNames + fuelConsumptionColumnNames)
+    fuelConsumptionData = relateVehicleTableToFuelConsumption(fuelConsumptionData)
+    fuelConsumptionData = dropVehicleColumnsToFuelConsumption(fuelConsumptionData)
+    fuelConsumptionData.to_sql('tbl_fuelConsumption', con=conn, if_exists='replace', index=True, index_label='id')
 
 
-def queryPerClass(clase):
+def findAllVehicles():
     query = """ SELECT *
                 FROM "tbl_vehicle" """
-                # WHERE "Class" = %(clase)s """
-    # queryParameters = {'clase': clase}
-    # dataQuery = pd.read_sql_query(query, con=getConnection(), params=queryParameters)
-    dataQuery = pd.read_sql_query(query, con=getConnection())
-    return dataQuery
+    return pd.read_sql_query(query, con=getConnection())
+
+
+def findFuelConsumptionRatings():
+    query = """ SELECT *
+                FROM "tbl_fuelConsumption" """
+    print(pd.read_sql_query(query, con=getConnection()))
 
 
 if __name__ == '__main__':
-    loadDB()
-    print(queryPerClass(""))
+    fileName = "D:\\Users\\jhonf\\Documents\\Programacion\\Codigo\\Python\\ai-fuel-consumption-ratings\\src\\main" \
+           "\\resoures\\MY2022FuelConsumptionRatings.csv"
+    loadVehicleTable(fileName)
+    loadFuelConsumptionTable(fileName)
+    findFuelConsumptionRatings()
