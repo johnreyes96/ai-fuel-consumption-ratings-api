6610556be466048545daa85d6c0b5ed12988f093
johnfreyes <jhonfer96@hotmail.com>
2022-05-15 15:18:35 -0500

    add two tables from one cvs file

--- src/main/python/classification/main.py
+++ src/main/python/classification/main.py
@@ -7,10 +7,10 @@ def getConnection():
     return create_engine('postgresql://postgres:postgrespw@localhost:49153/')
 
 
-def dropTable():
+def dropTable(table):
     conn = getConnection()
     conn.execute(DropTable(
-        Table('temp_vehicle', MetaData(), schema='public')
+        Table(table, MetaData(), schema='public')
     ))
 
 
@@ -18,15 +18,23 @@ def loadDB():
     conn = getConnection()
     file = "D:\\Users\\jhonf\\Documents\\Programacion\\Codigo\\Python\\ai-fuel-consumption-ratings\\src\\main" \
            "\\resoures\\MY2022FuelConsumptionRatings.csv"
-    datos = pd.read_csv(file, usecols=['strModelYear', 'strMake', 'strModel', 'strClass'])
-    dropTable()
-    datos = datos.drop_duplicates()
-    datos.to_sql('temp_vehicle', con=conn)
+    vehicleColumnNames = ['strModelYear', 'strMake', 'strModel', 'strClass']
+    fuelConsumptionColumnNames = ['douEngineSize_L', 'intCylinders', 'strTransmission', 'strFuelType',
+                                  'douFuelConsumption_City_L-100km', 'douFuelConsumption_Hwy_L-100km',
+                                  'douFuelConsumption_Comb_L-100km', 'intFuelConsumption_Comb_mpg',
+                                  'intCO2Emissions_g-km', 'intCO2Rating', 'intSmogRating']
+    vehicleData = pd.read_csv(file, usecols=vehicleColumnNames)
+    fuelConsumptionData = pd.read_csv(file, usecols=fuelConsumptionColumnNames)
+    dropTable('tbl_vehicle')
+    dropTable('tbl_fuelConsumption')
+    vehicleData = vehicleData.drop_duplicates()
+    vehicleData.to_sql('tbl_vehicle', con=conn)
+    fuelConsumptionData.to_sql('tbl_fuelConsumption', con=conn)
 
 
 def queryPerClass(clase):
     query = """ SELECT *
-                FROM "temp_vehicle" """
+                FROM "tbl_vehicle" """
     dataQuery = pd.read_sql_query(query, con=getConnection())
     return dataQuery
 
