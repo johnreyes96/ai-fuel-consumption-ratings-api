920e30f49143e1102113f6a3c8c5a8dc81fd6f91
johnfreyes <jhonfer96@hotmail.com>
2022-05-30 14:34:08 -0500

    add person stats service with analitics lineal regression using pearson algorithms

--- src/main/python/regression/main.py
+++ src/main/python/regression/main.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 import pandas as pd
 from flask import Flask, jsonify
+from scipy import stats
 
 
 app = Flask(__name__)
@@ -73,12 +74,23 @@ def findAllVehicles():
     return pd.read_sql_query(query, con=getConnection())
 
 
+def findAllDataSet():
+    query = """ SELECT ve."strModelYear", ve."strMake", ve."strModel", ve."strClass", fc."douEngineSize_L",
+                fc."intCylinders", fc."strTransmission", fc."strFuelType", fc."douFuelConsumption_City_L-100km",
+                fc."douFuelConsumption_Hwy_L-100km", fc."douFuelConsumption_Comb_L-100km",
+                fc."intFuelConsumption_Comb_mpg", fc."intCO2Emissions_g-km", fc."intCO2Rating", fc."intSmogRating"
+                FROM "tbl_fuelConsumption" fc
+                INNER JOIN tbl_vehicle ve ON ve.id = fc."vehicleId" """
+    pd.set_option('display.max_columns', 15)
+    return pd.read_sql_query(query, con=getConnection())
+
+
 def findFuelConsumptionRatings():
     query = """ SELECT ve."strModelYear", ve."strMake", ve."strModel", ve."strClass", fc."douEngineSize_L",
-        fc."intCylinders", fc."strTransmission", fc."strFuelType", fc."douFuelConsumption_City_L-100km",
-        fc."intCO2Emissions_g-km", fc."intCO2Rating"
-        FROM "tbl_fuelConsumption" fc
-        INNER JOIN tbl_vehicle ve ON ve.id = fc."vehicleId" """
+                fc."intCylinders", fc."strTransmission", fc."strFuelType", fc."douFuelConsumption_City_L-100km",
+                fc."intCO2Emissions_g-km", fc."intCO2Rating"
+                FROM "tbl_fuelConsumption" fc
+                INNER JOIN tbl_vehicle ve ON ve.id = fc."vehicleId" """
     pd.set_option('display.max_columns', 15)
     return pd.read_sql_query(query, con=getConnection())
 
@@ -94,6 +106,29 @@ def populateDB():
     dataFrame = findFuelConsumptionRatings()
     response = {
         'data': dataFrame.to_json(orient='index')
+    }
+    return jsonify(response)
+
+
+@app.route('/person-stats', methods=['GET'])
+def getPersonRStats():
+    dataFrame = findAllDataSet()
+    pearsonCoef, p_value = stats.pearsonr(dataFrame['intCO2Emissions_g-km'], dataFrame['douFuelConsumption_Comb_L-100km'])
+    stat1 = "The Pearson Correlation Coefficient of CO2 Emissions(g/km) vs Fuel Consumption(Comb (L/100 km)) is" + str(pearsonCoef) + " with a P-value of P =" + str(p_value)
+    pearsonCoef, p_value = stats.pearsonr(dataFrame['intCO2Rating'], dataFrame['douFuelConsumption_Comb_L-100km'])
+    stat2 = "The Pearson Correlation Coefficient of CO2 Rating vs Fuel Consumption(Comb (L/100 km)) is" + str(pearsonCoef) + " with a P-value of P =" + str(p_value)
+    pearsonCoef, p_value = stats.pearsonr(dataFrame['intSmogRating'], dataFrame['douFuelConsumption_Comb_L-100km'])
+    stat3 = "The Pearson Correlation Coefficient of Smog Rating vs Fuel Consumption(Comb (L/100 km)) is" + str(pearsonCoef) + " with a P-value of P =" + str(p_value)
+    pearsonCoef, p_value = stats.pearsonr(dataFrame['intCylinders'], dataFrame['douFuelConsumption_Comb_L-100km'])
+    stat4 = "The Pearson Correlation Coefficient of Cylinders vs  Combined City & Highway Fuel Consumption is" + str(pearsonCoef) + " with a P-value of P =" + str(p_value)
+    pearsonCoef, p_value = stats.pearsonr(dataFrame['douEngineSize_L'], dataFrame['douFuelConsumption_Comb_L-100km'])
+    stat5 = "The Pearson Correlation Coefficient Engine Size vs Combined City & Highway Fuel Consumption is" + str(pearsonCoef) + " with a P-value of P =" + str(p_value)
+    response = {
+        'CO2 Emissions(g/km)': stat1,
+        'CO2 Rating': stat2,
+        'Smog Rating': stat3,
+        'Cylinders': stat4,
+        'Engine Size': stat5
     }
     return jsonify(response)
 
